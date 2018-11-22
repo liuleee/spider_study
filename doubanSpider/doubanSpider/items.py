71171d2ceb94bae6fc11f6c89550526063c22ae8
liulee <helloliulee@gmail.com>
2018-11-22 19:26:29 +0800

    first commit

---
+++ doubanSpider/doubanSpider/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DoubanspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    #序号
+    serial_num = scrapy.Field()
+    movie_name = scrapy.Field()
+    introduce = scrapy.Field()
+    star = scrapy.Field()
+    #评论
+    evaluate = scrapy.Field()
+    #描述
+    describe = scrapy.Field()
+
